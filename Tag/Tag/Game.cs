53802442a6d731fa40cf3899c5ef8108252d5ba9
Deadcreep <tolla.von@gmail.com>
2017-02-20 19:37:38 +0500

    Add files via upload

---
+++ Tag/Tag/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tag
+{
+    class Game
+    {
+        Field field;
+
+        public int GetLocation(int value)
+        {
+            int location = 0;
+            throw new System.NotImplementedException();
+            return location;
+        }
+
+        
+        public void Shift()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
